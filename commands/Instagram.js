81e6caccaf46e1637e37673d68364b0a4c12b168
Y6sf627u9sg <131579865+Y6sf627u9sg@users.noreply.github.com>
2023-05-06 21:07:08 +0300

    Update Instagram.js

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -1,14 +1,14 @@
 const { Insta,cmd } = require('../lib')
 cmd({
-        pattern: "insta",
+        pattern: "انستا",
         desc: "download instagram post.",
         category: "downloader",
         filename: __filename
     },
     async(Void, citel,text,{isCreator}) => {
-if(!text) return citel.reply('Need post url.')
+if(!text) return citel.reply('جيب رابط.')
 let response = await Insta(text)
 for (let i=0;i<response.length;i++) {
-await Void.sendFileUrl(citel.chat, response[i], `*Downloaded Media from instagram.*`, citel)
+await Void.sendFileUrl(citel.chat, response[i], `*التحميل من انستجرام.*`, citel)
 }
     });
